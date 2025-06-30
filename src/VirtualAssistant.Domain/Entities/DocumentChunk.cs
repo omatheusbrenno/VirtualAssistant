9d885e44d367af959e5fbde272db6a07ece1106e
omatheusbrenno <matheusbrennolanasilva@gmail.com>
2025-06-30 11:38:24 -0300

    feat: adding document chunk entitie

---
+++ src/VirtualAssistant.Domain/Entities/DocumentChunk.cs
@@ -0,0 +1,9 @@
+﻿namespace VirtualAssistant.Domain.Entities
+{
+    public class DocumentChunk
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+        public Dictionary<string, object> Metadata { get; set; } = new();
+    }
+}
