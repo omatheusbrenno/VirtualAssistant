213cf4c86d8bfd9a5b136d1ee4c7a3b006c569da
omatheusbrenno <matheusbrennolanasilva@gmail.com>
2025-06-30 11:03:47 -0300

    feat: adding user profile entitie

---
+++ src/VirtualAssistant.Domain/Entities/UserProfile.cs
@@ -0,0 +1,10 @@
+﻿namespace VirtualAssistant.Domain.Entities
+{
+    public class UserProfile
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; } // "Cidadao", "Colaborador"
+        public List<string> Permissions { get; set; } = new();
+    }
+}
