9900ce2e753c8226a2e0b587f8216e818e1d3baf
omatheusbrenno <matheusbrennolanasilva@gmail.com>
2025-06-30 16:31:06 -0300

    feat: adding chat service interface to text or audio messages

---
+++ src/VirtualAssistant.Application/Interfaces/IChatService.cs
@@ -0,0 +1,11 @@
+﻿using VirtualAssistant.Domain.Entities;
+
+namespace VirtualAssistant.Application.Interfaces;
+public interface ITextMessageProcessor
+{
+    Task<string> ProcessTextMessageAsync(string query, string conversationId, UserProfile user);
+}
+public interface IAudioMessageProcessor
+{
+    Task<byte[]> ProcessAudioMessageAsync(Stream audioStream, string fileName, string contentType, string conversationId, UserProfile user);
+}
