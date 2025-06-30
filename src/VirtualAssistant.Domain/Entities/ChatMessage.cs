f49dad9c4b170cf13ea365260df348a7169b38bf
omatheusbrenno <matheusbrennolanasilva@gmail.com>
2025-06-30 11:19:38 -0300

    feat: adding chat message entitie

---
+++ src/VirtualAssistant.Domain/Entities/ChatMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace VirtualAssistant.Domain.Entities
+{
+    public class ChatMessage
+    {
+        public enum MessageAuthor { User, Assistant }
+        public MessageAuthor Author { get; set; }
+        public string Content { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    }
+}
