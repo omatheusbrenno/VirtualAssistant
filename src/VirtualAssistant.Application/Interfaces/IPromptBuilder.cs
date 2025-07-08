5dd4dc851ab53929e8f086a6efb8ca5e3b0c40dc
omatheusbrenno <matheusbrennolanasilva@gmail.com>
2025-07-08 12:39:57 -0300

    feat: adding prompt builder interface

---
+++ src/VirtualAssistant.Application/Interfaces/IPromptBuilder.cs
@@ -0,0 +1,8 @@
+﻿using VirtualAssistant.Domain.Entities;
+
+namespace VirtualAssistant.Application.Interfaces;
+
+public interface IPromptBuilder
+{
+    string Build(string query, IEnumerable<DocumentChunk> contextChunks, IEnumerable<ChatMessage> history, UserProfile user);
+}
