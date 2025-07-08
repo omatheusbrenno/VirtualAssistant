e49348e4aff6f25fdb1d4029b8dae68b4b4010e9
omatheusbrenno <matheusbrennolanasilva@gmail.com>
2025-07-08 12:59:52 -0300

    feat: adding context filter interface

---
+++ src/VirtualAssistant.Application/Interfaces/IContextFilter.cs
@@ -0,0 +1,7 @@
+﻿using VirtualAssistant.Domain.Entities;
+
+namespace VirtualAssistant.Application.Interfaces;
+public interface IContextFilter
+{
+    IEnumerable<DocumentChunk> Filter(IEnumerable<DocumentChunk> chunks, UserProfile user);
+}
