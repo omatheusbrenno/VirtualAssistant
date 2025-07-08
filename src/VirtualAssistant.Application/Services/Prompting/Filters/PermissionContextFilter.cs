0b51206adf030a6fcc7ffe6ca4adf1b799da0769
omatheusbrenno <matheusbrennolanasilva@gmail.com>
2025-07-08 13:16:22 -0300

    feat: adding permission role for context filter

---
+++ src/VirtualAssistant.Application/Services/Prompting/Filters/PermissionContextFilter.cs
@@ -0,0 +1,14 @@
+﻿using VirtualAssistant.Application.Interfaces;
+using VirtualAssistant.Domain.Entities;
+
+namespace VirtualAssistant.Application.Services.Prompting.Filters;
+
+public class PermissionContextFilter : IContextFilter
+{
+    public IEnumerable<DocumentChunk> Filter(IEnumerable<DocumentChunk> chunks, UserProfile user)
+    {
+        return chunks.Where(chunk =>
+            !chunk.Metadata.TryGetValue("permission", out var requiredPermission) ||
+            user.Permissions.Contains(requiredPermission.ToString()!));
+    }
+}
