7292dc8868a3161ca612944aeb75ecdcb9d3ea9f
omatheusbrenno <matheusbrennolanasilva@gmail.com>
2025-07-21 10:59:07 -0300

    feat: adding interface to generate embedding and response

---
+++ src/VirtualAssistant.Application/Interfaces/ILargeLanguageModel.cs
@@ -0,0 +1,8 @@
+﻿namespace VirtualAssistant.Application.Interfaces
+{
+    public interface ILargeLanguageModel
+    {
+        Task<float[]> GenerateEmbeddingAsync(string text, CancellationToken cancellationToken = default);
+        Task<string> GenerateResponseAsync(string prompt, CancellationToken cancellationToken = default);
+    }
+}
