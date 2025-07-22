326cc250274cd5084a8f9e3834973f458c4589bf
omatheusbrenno <matheusbrennolanasilva@gmail.com>
2025-07-22 15:46:33 -0300

    feat: adding Score property to DocumentChunk

--- src/VirtualAssistant.Domain/Entities/DocumentChunk.cs
+++ src/VirtualAssistant.Domain/Entities/DocumentChunk.cs
@@ -5,5 +5,6 @@
         public Guid Id { get; set; }
         public string Content { get; set; }
         public Dictionary<string, object> Metadata { get; set; } = new();
+        public float? Score { get; set; }
     }
 }
